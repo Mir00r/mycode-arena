632a5d8c67b42a846d1c025fa0a666d29c13582b
Mir00r <abdur.razzaq06@gmail.com>
2015-11-21 15:17:04 +0600

    Tree: Inorder Traversal

---
+++ Data_Structure/Trees/Tree-Traverssals/Inorder_Travarsal.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define LI long int
+#define LLI long long int
+#define LL __int64
+#define ULL unsigned long long
+#define LLU long long unsigned
+#define row 205
+#define col 205
+#define MAX 1006
+#define jora pair <int, int>
+#define memo(array, value) memset(array, value, sizeof(array))
+#define pb push_back
+#define NL puts ("")
+#define inf (1 << 28)
+#define eps 1e9
+#define mod 7477777
+#define PI 3.1415926535897932384626433832795
+
+
+// A binary tree node has data, pointer to left child and pointer to right child
+
+struct Node{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+
+
+// this method allocate a new Node with the given data with NULL left and right pointer
+
+struct Node* NewNode(int value){
+	struct Node *node = (struct Node*)malloc ( sizeof (struct Node) ); // Creating a new Node 
+	
+	node->data = value;
+	node->left = NULL;
+	node->right = NULL;
+	
+	return (node);
+}
+
+
+// Givern a binary tree, print it's node's in Inorder
+void Inorder (struct Node *node){
+	if (node == NULL)
+		return;
+		
+	// recure the laft child node subtree 
+	Inorder (node->left);
+	
+	// first print root node 
+	printf ("%d ", node->data);
+	
+	// recure the right child node subtree
+	Inorder (node->right);
+	
+}
+
+int main (){
+	struct Node *root;
+	
+	root = NewNode (1);
+	root->left = NewNode (2);
+	root->right = NewNode (3);
+	root->left->left = NewNode (4);
+	root->left->right = NewNode (5);	
+	
+	Inorder (root);
+	
+	return 0;
+}
+
+
